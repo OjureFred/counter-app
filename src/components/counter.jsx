df5b9c8a6698975f3af72a79bee84210e2de3a1b
Fredrick Ojure <fredojure@gmail.com>
2022-08-26 19:22:23 +0300

    3. Created method to handle increment button click and render list of tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,10 +25,11 @@ class Counter  extends React.Component {
             <span style = {this.style} className={ this.getBadgeClasses() }>
                 {this.formatCount()}
             </span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map( tag => <li>{ tag }</li>)}
-            </ul>
+            <button onClick= {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <div>
+                {this.state.tags.length === 0 && "Please create a new tag!" }
+                {this.renderTags()}
+            </div>
         </div>
         );
     }
@@ -37,6 +38,15 @@ class Counter  extends React.Component {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>
+        return <ul>{this.state.tags.map( tag => <li key={tag}>{ tag }</li>)}</ul>
+    }
+
+    handleIncrement() {
+        console.log('Increment clicked')
     }
 }
  
