79933ac25889f5c7d478cf1ee8061764f50a38a5
Fredrick Ojure <fredojure@gmail.com>
2022-08-26 17:46:20 +0300

    1. Created app skeleton and created the Counter class to act as parent class

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Counter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
