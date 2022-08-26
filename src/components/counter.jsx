cd3e64dd7a2e65b42aeca170fc07089976cf65b6
Fredrick Ojure <fredojure@gmail.com>
2022-08-26 19:07:17 +0300

    2. Created formatting and styling methods within the class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,40 @@ import React, { Component } from 'react';
 
 class Counter  extends React.Component {
     state = {
-        count:0
-      } ;
-    render() { 
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3'],
+        imageUrl: 'https://picsum.photos/200'
+      };
+    
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold',
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? <h1>zero</h1> : count;
+    }
+    render() {
+        let classes = this.getBadgeClasses();
         return (
         <div>
-            <span>{this.state.count}</span>
-            <button>Increment</button>
+            <img src={this.state.imageUrl} alt="" />
+            <span style = {this.style} className={ this.getBadgeClasses() }>
+                {this.formatCount()}
+            </span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map( tag => <li>{ tag }</li>)}
+            </ul>
         </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 }
  
