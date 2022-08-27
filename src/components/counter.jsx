e3788cbcd39506a85efef81859648150d663cf2d
Fredrick Ojure <fredojure@gmail.com>
2022-08-27 16:29:31 +0300

    5. Created counters component. Added 4 counter components to it. Wired event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
 
+
 class Counter  extends React.Component {
-    state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3'],
-        imageUrl: 'https://picsum.photos/200'
-      };
-    
+     
     constructor() {
         super()
         this.handleIncrement = this.handleIncrement.bind(this)
@@ -19,20 +15,22 @@ class Counter  extends React.Component {
     }
 
     formatCount(){
-        const { count } = this.state;
+        const { count } = this.props.Counter
         return count === 0 ? <h3>zero</h3> : count;
     }
     render() {
+        console.log('props', this.props)
         let classes = this.getBadgeClasses();
         return (
         <div>
             <img src={this.state.imageUrl} alt="" />
-            
+            {this.props.children}
             <span style = {this.style} className={ this.getBadgeClasses() }>
                 
             </span>
             <p>{this.formatCount()}</p>
-            <button onClick= {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick= {() => this.props.onIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={()=>this.props.onDelete(this.props.Counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             <div>
             
 
@@ -54,8 +52,6 @@ class Counter  extends React.Component {
         return <ul>{this.state.tags.map( tag => <li key={tag}>{ tag }</li>)}</ul>
     }
 
-    handleIncrement = () => {
-        this.setState({ count: this.state.count + 1});
-}
+   
 }
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(<Counters />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
